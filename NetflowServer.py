c395f56ba00d7b5c46491c3b350f7967dd450f54
Nikolay Bogdanov <gyrt@cs.klan-hub.ru>
2013-11-14 21:53:45 +0400

    Code refactoring

---
+++ NetflowServer.py
@@ -0,0 +1,38 @@
+import SocketServer
+import netflow
+import argparse
+import threading
+import NetflowServer
+
+class ThreadingUDPServer(SocketServer.ThreadingMixIn, SocketServer.UDPServer): pass
+
+class NetflowUDPHandler(SocketServer.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request[0]
+        print("{0} wrote:".format(self.client_address[0]))
+        nf = netflow.Netflow5(self.data)
+
+	print(nf.version)
+	print(nf.data)
+
+        # just send back the same data, but upper-cased
+        #self.request.sendall(self.data.upper())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9996
+
+    # Create the server, binding to localhost on port 9999
+    server = ThreadingUDPServer((HOST, PORT), MyUDPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import SocketServer
 import netflow
-
-class MyUDPHandler(SocketServer.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
-
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request[0]
-        print("{0} wrote:".format(self.client_address[0]))
-        nf = netflow.Netflow5(self.data)
-
-	print(nf.version)
-	print(nf.data)
-
-        # just send back the same data, but upper-cased
-        #self.request.sendall(self.data.upper())
+import argparse
+import threading
+import NetflowServer
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9996
+	HOST, PORT = "localhost", 9996
+	ACTION = "/bin/true"
+	LIMIT = 4294967296
 
-    # Create the server, binding to localhost on port 9999
-    server = SocketServer.UDPServer((HOST, PORT), MyUDPHandler)
+	parser = argparse.ArgumentParser("sphx-server")
+	parser.add_argument("-d", "--debug", help="Start in debug mode", action="store_true", default=False)
+	parser.add_argument("-i", "--ip", help="IP to listen (default: {0})".format(HOST), type=str, default=HOST)
+	parser.add_argument("-p", "--port", help="PORT to listen (default: {0})".format(PORT), type=int, default=PORT)
+	parser.add_argument("-a", "--action", help="Action for host reached the limit (default: {0})".format(ACTION), type=str, default=ACTION)
+	parser.add_argument("-l", "--limit", help="Packates limit (default: {0})".format(LIMIT), type=int, default=LIMIT)
+	args = parser.parse_args()
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+	
+
+	# Create the server, binding to localhost on port 9999
+	server = NetflowServer.ThreadingUDPServer((HOST, PORT), NetflowServer.NetflowUDPHandler)
+
+	# Activate the server; this will keep running until you
+	# interrupt the program with Ctrl-C
+	server.serve_forever()
